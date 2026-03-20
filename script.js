7668a3899e3066b7e34a99ed11a10ca8e9801bba
dismantled1 <aayushman.pratap.singh23@gmail.com>
2026-03-20 21:44:37 +0530

    Changed JavaScript

--- script.js
+++ script.js
@@ -142,7 +142,7 @@ window.addEventListener('scroll', () => {
             const scrollVal = window.scrollY;
             const bg = document.querySelector('.bg-image');
             if (bg) {
-                bg.style.transform = `translateY(${scrollVal * 0.3}px)`;
+                bg.style.transform = `translateY(${scrollVal * 0.11}px)`;
             }
             ticking = false;
         });
